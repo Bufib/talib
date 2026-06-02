f5f329eb18a6932849e1af3d4aa3931ab7e03500
Bufib <bufib.bufib@gmail.com>
2026-06-02 22:33:27 +0200

    fix

--- src/components/app-tabs.tsx
+++ src/components/app-tabs.tsx
@@ -13,8 +13,8 @@ export default function AppTabs() {
       backgroundColor={Colors[colorScheme].contrast}
       indicatorColor={Colors[colorScheme].background}
       labelStyle={{
-        selected: { color: Colors[colorScheme].tint, fontSize: 15 },
-        default: { color: Colors[colorScheme].text, fontSize: 15},
+        selected: { color: Colors[colorScheme].tint, fontSize: 18 },
+        default: { color: Colors[colorScheme].text, fontSize: 18},
       }}
     >
       <NativeTabs.Trigger name="home" disableTransparentOnScrollEdge>
